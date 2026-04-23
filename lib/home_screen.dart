521c66dd7344e4e11b630629f9aa856ad2aeb60a
umerDev74 <umern7429@gmail.com>
2026-04-23 14:31:10 +0500

    apis intro

---
+++ lib/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("APIs Course"),
+      ),
+      body: Column(children: [
+
+      ],),
+    );
+  }
+}
